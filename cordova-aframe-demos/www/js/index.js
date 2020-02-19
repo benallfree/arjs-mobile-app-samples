5e5cf3d6751fbf9ccba26620abb95560f2d7ec00
Ben Allfree <ben@benallfree.com>
2020-02-19 10:17:10 -0800

    A-frame demos update

--- cordova-aframe-demos/www/js/index.js
+++ cordova-aframe-demos/www/js/index.js
@@ -2,18 +2,9 @@
   'use strict';
 
   document.addEventListener("deviceready", function() {
-    // Just for iOS devices.
-    if (cordova.platformId === "ios") {
-      cordova.plugins.iosrtc.registerGlobals();
+    console.log("Cordova is ready.");
 
-      // load adapter.js
-      var script = document.createElement("script");
-      script.type = "text/javascript";
-      script.src = "./js/adapter-latest.js";
-      script.async = false;
-      document.getElementsByTagName("head")[0].appendChild(script);
-      document.write(document.getElementById("aframe").innerHTML);
-    }
+    document.write(document.getElementById("aframe").innerHTML);
   });
 
 }());
