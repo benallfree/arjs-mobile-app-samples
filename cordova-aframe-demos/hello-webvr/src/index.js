45b7cbf5a02b839bc50b15b76eaf5f3741e6ea3e
Ben Allfree <ben@benallfree.com>
2020-02-26 16:43:41 -0800

    Move a-frame demos to separate repo

--- cordova-aframe-demos/hello-webvr/src/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("deviceready", function() {
-  console.log("Cordova is ready.");
-
-  document.getElementById("aframe_root").innerHTML = document.getElementById(
-    "aframe_init"
-  ).innerHTML;
-});
